5fec7b1fb90687b7c399a9e0c0d86a8363dfd62b
Voidraizer <splutikoff@gmail.com>
2015-08-30 20:48:57 -0400

    Started sun script

---
+++ Assets/Scripts/SunMotion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunMotion : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3[] sunWPs;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
