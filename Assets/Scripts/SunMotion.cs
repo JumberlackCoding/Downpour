48c1b26fec216419b9a6bbdafde40d308d3fbecb
Voidraizer <splutikoff@gmail.com>
2015-08-30 23:18:59 -0400

    Added sun waypoints
    Set up sun for inspector control

--- Assets/Scripts/SunMotion.cs
+++ Assets/Scripts/SunMotion.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class SunMotion : MonoBehaviour {
 
     [SerializeField]
-    private Vector3[] sunWPs;
+    private Transform[] sunWaypoints;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private int currentWaypoint;
 
 	// Use this for initialization
 	void Start () {
-
+        currentWaypoint = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        Vector3 targetDirection = new Vector3( sunWaypoints[currentWaypoint].position.x - transform.position.x, sunWaypoints[currentWaypoint].position.y - transform.position.y, 0 );
+        if( transform.position.x >= sunWaypoints[currentWaypoint].position.x )
+        {
+            currentWaypoint++;
+        }
+        transform.Translate( targetDirection.normalized * Time.deltaTime * moveSpeed );
 	}
 }
